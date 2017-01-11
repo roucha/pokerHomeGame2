0021b0fd84a61fb30bbd39fa2d17f36265b68584
Vlad Barash <vladb@Vlads-MacBook-Pro.local>
2017-01-11 13:55:48 -0500

    Working Version 1
    Finally

--- pokerHomeGame2/ComputeViewController.swift
+++ pokerHomeGame2/ComputeViewController.swift
@@ -39,12 +39,12 @@ class ComputeViewController: UIViewController {
     
     
     @IBAction func compute(_ sender: Any) {
+        // TODO:
+        // Iff total starting chip count == total ending chip count
+        // TODO:
         
-        //Iff total starting chip count == total ending chip count
         
-        
-        
-        //create sorted arrays of values (dictionaries are unsorted)
+        // create sorted arrays of values (dictionaries are unsorted)
         
         var nameArray = [String]()
         var startArray = [Double]()
@@ -54,22 +54,40 @@ class ComputeViewController: UIViewController {
         nameArray.append(name1.text!)
         nameArray.append(name2.text!)
         nameArray.append(name3.text!)
+        nameArray.append(name4.text!)
+        nameArray.append(name5.text!)
+        nameArray.append(name6.text!)
+        nameArray.append(name7.text!)
+        nameArray.append(name8.text!)
         
         startArray.append(Double(start1.text!)!)
         startArray.append(Double(start2.text!)!)
         startArray.append(Double(start3.text!)!)
+        startArray.append(Double(start4.text!)!)
+        startArray.append(Double(start5.text!)!)
+        startArray.append(Double(start6.text!)!)
+        startArray.append(Double(start7.text!)!)
+        startArray.append(Double(start8.text!)!)
         
         endArray.append(Double(end1.text!)!)
         endArray.append(Double(end2.text!)!)
         endArray.append(Double(end3.text!)!)
+        endArray.append(Double(end4.text!)!)
+        endArray.append(Double(end5.text!)!)
+        endArray.append(Double(end6.text!)!)
+        endArray.append(Double(end7.text!)!)
+        endArray.append(Double(end8.text!)!)
         
         difference[nameArray[0]] = endArray[0] - startArray[0]
         difference[nameArray[1]] = endArray[1] - startArray[1]
         difference[nameArray[2]] = endArray[2] - startArray[2]
+        difference[nameArray[3]] = endArray[3] - startArray[3]
+        difference[nameArray[4]] = endArray[4] - startArray[4]
+        difference[nameArray[5]] = endArray[5] - startArray[5]
+        difference[nameArray[6]] = endArray[6] - startArray[6]
+        difference[nameArray[7]] = endArray[7] - startArray[7]
+        difference[nameArray[8]] = endArray[8] - startArray[8]
         
-
-//        print(difference)
-//        print("****")
         
         var nameSorted = [String]()
         var differenceSorted = [Double]()
@@ -81,14 +99,10 @@ class ComputeViewController: UIViewController {
         
         print(nameSorted)
         print(differenceSorted)
+        print("***")
         
         
-        
-        
-        
-        //loop through and determine who owes who what
-        
-        // y is equal to the last value's position in the sorted array (largest chip size)
+        // loop through and determine who owes who what
         
         var y = differenceSorted.count - 1
         
@@ -105,7 +119,7 @@ class ComputeViewController: UIViewController {
                     print(nameSorted[y])
                     
                     differenceSorted[i] += differenceSorted[y]
-                    //differenceSorted[y] -= differenceSorted[y]
+                    differenceSorted[y] -= differenceSorted[y]
                     
                 } else if abs(differenceSorted[i]) <= differenceSorted[y] {
                     
@@ -115,32 +129,19 @@ class ComputeViewController: UIViewController {
                     print("to")
                     print(nameSorted[y])
                     
-                    differenceSorted[i] += differenceSorted[y]
-                    //differenceSorted[y] -= differenceSorted[y]
+                    differenceSorted[y] -= abs(differenceSorted[i])
+                    differenceSorted[i] += abs(differenceSorted[i])
                     
                 }
                 
+                if differenceSorted[y] == 0 {
+                    y-=1
+                }
+                
+                print("\n")
             }
             
-            y-=1
-            
         }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
         
     }
     
@@ -148,7 +149,6 @@ class ComputeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("hi")
     }
     
     override func didReceiveMemoryWarning() {
