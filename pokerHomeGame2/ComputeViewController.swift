1c899fc9e93e14b65fd2343cfe429b8361b0732e
Vlad Barash <vladb@Vlads-MacBook-Pro.local>
2017-01-11 11:32:43 -0500

    2
    2

---
+++ pokerHomeGame2/ComputeViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ComputeViewController.swift
+//  pokerHomeGame2
+//
+//  Created by Vlad Barash on 1/11/17.
+//  Copyright © 2017 Vlad Barash. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ComputeViewController: UIViewController {
+    
+    
+    @IBOutlet weak var name1: UITextField!
+    @IBOutlet weak var start1: UITextField!
+    @IBOutlet weak var end1: UITextField!
+    @IBOutlet weak var name2: UITextField!
+    @IBOutlet weak var start2: UITextField!
+    @IBOutlet weak var end2: UITextField!
+    @IBOutlet weak var name3: UITextField!
+    @IBOutlet weak var start3: UITextField!
+    @IBOutlet weak var end3: UITextField!
+    @IBOutlet weak var name4: UITextField!
+    @IBOutlet weak var start4: UITextField!
+    @IBOutlet weak var end4: UITextField!
+    @IBOutlet weak var name5: UITextField!
+    @IBOutlet weak var start5: UITextField!
+    @IBOutlet weak var end5: UITextField!
+    @IBOutlet weak var name6: UITextField!
+    @IBOutlet weak var start6: UITextField!
+    @IBOutlet weak var end6: UITextField!
+    @IBOutlet weak var name7: UITextField!
+    @IBOutlet weak var start7: UITextField!
+    @IBOutlet weak var end7: UITextField!
+    @IBOutlet weak var name8: UITextField!
+    @IBOutlet weak var start8: UITextField!
+    @IBOutlet weak var end8: UITextField!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("hi")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
